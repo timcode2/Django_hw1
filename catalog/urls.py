f4b1079ecf1e6755fb67c1a6839aa93b1daf4638
artemm <artem.m2013@gmail.com>
2023-08-29 20:25:03 +0300

    complete

---
+++ catalog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from catalog.views import home, contact
+
+
+urlpatterns = [
+    path('', home),
+    path('contacts/', contact)
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "catalog/home.html")
+
+
+def contact(request):
+    return render(request, "catalog/contact.html")
