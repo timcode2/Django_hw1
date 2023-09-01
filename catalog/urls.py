005b431f4d9a36972184be140869d66fc737bdeb
Artem Matrashov <artem.m2013@gmail.com>
2023-09-01 08:31:53 +0300

    fix some options

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from catalog.views import home, contact
@@ -6,4 +8,4 @@ from catalog.views import home, contact
 urlpatterns = [
     path('', home),
     path('contacts/', contact)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
