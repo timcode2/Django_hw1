25d5c7bda7e4c938e3246cc9bb8298e757b9ba23
Artem Matrashov <artem.m2013@gmail.com>
2023-09-23 10:14:40 +0300

    change from FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index_home, index_contact, index_product
+from catalog.views import index_contact, ProductListView, ProductDetailsView
 
 urlpatterns = [
-    path('', index_home, name='home'),
+    path('', ProductListView.as_view(), name='home'),
     path('contacts/', index_contact, name='contact'),
-    path('product/<int:pk>', index_product, name='product'),
+    path('product/<int:pk>', ProductDetailsView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,31 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from catalog.models import Product
 
 
-def index_home(request):
-    product = Product.objects.all()
-    context = {
-        'object_list': product
-    }
-    return render(request, "catalog/home.html", context)
+# def index_home(request):
+#     product = Product.objects.all()
+#     context = {
+#         'object_list': product
+#     }
+#     return render(request, "catalog/product_list.html", context)
+class ProductListView(ListView):
+    model = Product
 
 
 def index_contact(request):
-    return render(request, "catalog/contact.html")
+    return render(request, "catalog/contact_page.html")
 
 
-def index_product(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {
-        'object': product,
-        'title': f'Товар - {product.name}'
-    }
-    return render(request, "catalog/product.html", context)
+class ProductDetailsView(DetailView):
+    model = Product
+
+
+# def index_product(request, pk):
+#     product = Product.objects.get(pk=pk)
+#     context = {
+#         'object': product,
+#         'title': f'Товар - {product.name}'
+#     }
+#     return render(request, "catalog/product_detail.html", context)
