98476d1859838e248ed6138ad146449b6ab41452
Artem Matrashov <artem.m2013@gmail.com>
2023-09-26 02:38:07 +0300

    clean commits

--- catalog/views.py
+++ catalog/views.py
@@ -4,12 +4,6 @@ from django.views.generic import DetailView, ListView
 from catalog.models import Product
 
 
-# def index_home(request):
-#     product = Product.objects.all()
-#     context = {
-#         'object_list': product
-#     }
-#     return render(request, "catalog/product_list.html", context)
 class ProductListView(ListView):
     model = Product
 
@@ -20,12 +14,3 @@ def index_contact(request):
 
 class ProductDetailsView(DetailView):
     model = Product
-
-
-# def index_product(request, pk):
-#     product = Product.objects.get(pk=pk)
-#     context = {
-#         'object': product,
-#         'title': f'Товар - {product.name}'
-#     }
-#     return render(request, "catalog/product_detail.html", context)
