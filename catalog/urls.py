a3ca937ee201ba41ad49601760a1a3f095bb20e2
Artem Matrashov <artem.m2013@gmail.com>
2023-09-29 03:17:34 +0300

    add urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index_contact, ProductListView, ProductDetailsView
+from catalog.views import index_contact, ProductListView, ProductDetailsView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', index_contact, name='contact'),
     path('product/<int:pk>', ProductDetailsView.as_view(), name='product'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('edit/<int:pk>', ProductUpdateView.as_view(), name='product_edit'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
 ]
